b27cddde0f7ec7518a9da4da9c408586c93bc919
Maximo <max1901@hotmail.com>
2021-05-23 01:53:42 -0500

    New

--- models/server.js
+++ models/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 class Server {
     constructor() {
         this.app = express()
-        this.port = process.env.PORT || 3000
+        this.port = process.env.PORT
         //rutas para modelos
         
         this.authPath = '/api/auth/login'
